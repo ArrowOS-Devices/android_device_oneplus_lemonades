a544cdbd4d0e7d8c10bebc890fa3eb76e1ed73a7
alk3pInjection <webmaster@raspii.tech>
2021-12-31 01:07:43 +0800

    lemonades: Initial skeleton tree
    Change-Id: I68586eb0c729a13608a9b5ec1b9bd002a53aeee9

---
+++ device.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2020-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/oneplus/kebab/device.mk)
+
+# Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    device/oneplus/lemonades
